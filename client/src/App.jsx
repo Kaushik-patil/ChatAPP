1ddea2c01d257b352c206879a32a6326e4859d60
Kaushik Patil <kaushik1234567patil@gmail.com>
2025-04-08 07:35:43 -0400

    fixed lazy loading component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,34 @@
 import React, { useEffect, lazy, Suspense } from 'react';
 import './index.css';
-const Navbar = lazy (()=>import('./components/Navbar'));
+const Navbar = lazy(() => import('./components/Navbar'));
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-const HomePage = lazy(() => import('./Pages/HomePage'));
-const SignUpPage = lazy(()=>import( './Pages/SignUpPage'));
-const LoginPage  = lazy(()=>import('./Pages/LoginPage'  ));         
-const SettingPage= lazy (()=>import('./Pages/SettingPage'));
-const ProfilePage = lazy(()=>import('./Pages/ProfilePage'));
+// const HomePage = lazy(() => import('./Pages/HomePage'));
+// const SignUpPage = lazy(()=>import( './Pages/SignUpPage'));
+// const LoginPage  = lazy(()=>import('./Pages/LoginPage'  ));         
+// const SettingPage= lazy (()=>import('./Pages/SettingPage'));
+// const ProfilePage = lazy(()=>import('./Pages/ProfilePage'));
+
+import HomePage from './Pages/HomePage';
+import SignUpPage from './Pages/SignUpPage';
+import LoginPage from './Pages/LoginPage';
+import SettingPage from './Pages/SettingPage';
+import ProfilePage from './Pages/ProfilePage';
+
+
+
 import { useAuthStore } from './store/useAuthStore';
 import { useThemeStore } from './store/useThemeStore';
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 
-const fallBackLoader = ()=>{
-   return( <div className='flex items-center justify-center h-screen'>
-      <Loader className='size-10 animate-spin'></Loader>
-   </div>
-   );
-}
+// const fallBackLoader = ()=>{
+//    return( <div className='flex items-center justify-center h-screen'>
+//       <Loader className='size-10 animate-spin'></Loader>
+//    </div>
+//    );
+// }
 
 
 function App() {
@@ -47,54 +56,14 @@ function App() {
 
          <div >
 
-            <Navbar/>
-             {/* <Suspense fallback={fallBackLoader()}>
-               <Navbar/>
-            </Suspense> */}
-            {/* <Routes>
+            <Navbar />
+            <Routes>
                <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-
-               <Route path='/signup' element={!authUser ?
-                  <Suspense fallback={fallBackLoader()}>
-                     <SignUpPage />
-                  </Suspense>
-                  : <Navigate to="/" />} />
-
-               <Route path='/login' element={!authUser ?
-                  <Suspense fallback={fallBackLoader()}>
-                     <LoginPage />
-                  </Suspense>
-                  : <Navigate to="/" />} />
-
-               <Route path='/settings' element={
-                  <Suspense fallback={fallBackLoader()}>
-                     <SettingPage />
-                  </Suspense>
-               } />
-
-
-               <Route path='/profile' element={authUser ?
-
-                  <Suspense fallback={fallBackLoader()}>
-                     <ProfilePage />
-                  </Suspense>
-                  : <Navigate to="/login" />} />
-
-            </Routes> */}
-
-
-            <Suspense fallback={<fallBackLoader/>}>
-  <Routes>
-    <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-    <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-    <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-    <Route path='/settings' element={<SettingPage />} />
-    <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-  </Routes>
-</Suspense>
-
-
-
+               <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+               <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+               <Route path='/settings' element={<SettingPage />} />
+               <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+            </Routes>
             <Toaster />
          </div>
 
